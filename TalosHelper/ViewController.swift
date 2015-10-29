a82de64bdb740b752b0d2ff86cf2b245ce92d25a
Pavel Samarkin <psamarkin@mirantis.com>
2015-10-28 23:20:56 -0700

    Core functionality implemented

--- TalosHelper/ViewController.swift
+++ TalosHelper/ViewController.swift
@@ -1,18 +1,17 @@
-//
-//  ViewController.swift
-//  TalosHelper
-//
-//  Created by Pavel Samarkin on 10/28/15.
-//  Copyright © 2015 Pavel Samarkin. All rights reserved.
-//
-
 import UIKit
+
+private let hexAttributes: [String : AnyObject] = [
+    NSForegroundColorAttributeName : UIColor.blueColor()
+]
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var buttons: [UIButton]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        assert(buttons.count == 16, "Invalid nib")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +19,63 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private var decodedText = ""
+    private var hexCharacters = 0
+    private var currentHex = 0
 
+    private func refreshText() {
+        if hexCharacters == 2 {
+            decodedText.append(UnicodeScalar(currentHex))
+            currentHex = 0
+            hexCharacters = 0
+        }
+
+        let hex = NSAttributedString(string: currentHex.toHex(hexCharacters), attributes: hexAttributes)
+        let text = NSMutableAttributedString(string: decodedText)
+        text.appendAttributedString(hex)
+        textField.attributedText = text
+    }
+
+    @IBAction func buttonClick(sender: UIButton) {
+        guard let i = buttons.indexOf(sender) where 0...15 ~= i else {
+            print("invalid sender")
+            return
+        }
+
+        currentHex = (currentHex << 4) | i
+        hexCharacters++
+        refreshText()
+    }
+
+    @IBAction func backspaceClick(sender: AnyObject) {
+        if hexCharacters == 0 {
+            guard decodedText.characters.count > 0 else {
+                return
+            }
+            let lastChar: UnicodeScalar = decodedText.unicodeScalars.last!
+            decodedText = decodedText.withoutLastCharacter()
+            currentHex = Int(lastChar.value)
+            hexCharacters = 2
+        }
+        currentHex = currentHex >> 4
+        hexCharacters--
+        refreshText()
+    }
+}
+
+private extension Int {
+    func toHex(length: Int) -> String {
+        let s = String(format:"%0\(length)X", self).characters.suffix(length)
+        return String(s)
+    }
+}
+
+private extension String {
+    func withoutLastCharacter() -> String {
+        guard self.characters.count > 0 else {
+            return self
+        }
+        return self.substringToIndex(self.startIndex.advancedBy(self.characters.count-1))
+    }
 }
 
